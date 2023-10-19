897c7720fd25bfde7a05b697da342daa364e4da6
wangbin <wangbin@anji-plus.com>
2023-10-19 14:02:46 +0800

    gaea升级

--- report-core/src/main/java/com/anjiplus/template/gaea/business/config/BusinessAutoConfiguration.java
+++ report-core/src/main/java/com/anjiplus/template/gaea/business/config/BusinessAutoConfiguration.java
@@ -2,10 +2,30 @@ package com.anjiplus.template.gaea.business.config;
 
 import com.anjiplus.template.gaea.business.runner.ApplicationInitRunner;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.cache.ehcache.EhCacheCache;
-import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
+import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
+import org.springframework.boot.actuate.endpoint.web.*;
+import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * business配置类
@@ -26,5 +46,51 @@ public class BusinessAutoConfiguration {
     @Bean
     public ApplicationInitRunner applicationInitRunner() {
         return new ApplicationInitRunner();
+    }
+
+    @Bean
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Restful API")
+                .description("restful api")
+                .termsOfServiceUrl("https://www.anji-plus.com")
+                .contact(new Contact("aj-report","http://www.anji-plus.com","MS@anji-plus.com"))
+                .version("1.0")
+                .build();
+    }
+    @Bean
+    @ConditionalOnMissingBean
+    Docket docket(ApiInfo apiInfo){
+        return new Docket(DocumentationType.OAS_30).apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.anjiplus.product.dataway.modules"))
+                .paths(PathSelectors.any()).build();
+    }
+    /**
+     * 增加如下配置可解决Spring Boot 6.x 与Swagger 3.0.0 不兼容问题
+     **/
+    @Bean
+    public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(
+            WebEndpointsSupplier webEndpointsSupplier, ServletEndpointsSupplier servletEndpointsSupplier,
+            ControllerEndpointsSupplier controllerEndpointsSupplier,
+            EndpointMediaTypes endpointMediaTypes, CorsEndpointProperties corsProperties,
+            WebEndpointProperties webEndpointProperties, Environment environment) {
+        List<ExposableEndpoint<?>> allEndpoints = new ArrayList();
+        Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
+        allEndpoints.addAll(webEndpoints);
+        allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
+        allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
+        String basePath = webEndpointProperties.getBasePath();
+        EndpointMapping endpointMapping = new EndpointMapping(basePath);
+        boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(
+                webEndpointProperties, environment, basePath);
+        return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes,
+                corsProperties.toCorsConfiguration(), new EndpointLinksResolver(allEndpoints, basePath),
+                shouldRegisterLinksMapping, null);
+    }
+    private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties,
+                                               Environment environment, String basePath) {
+        return webEndpointProperties.getDiscovery().isEnabled()
+                && (StringUtils.hasText(basePath) || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
     }
 }
